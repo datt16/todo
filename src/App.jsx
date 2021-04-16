5b93f7a8ed655bb4a20503378712f8b5c90d4af4
datt16 <osatom.4639@gmail.com>
2021-04-16 10:55:07 +0000

    最低限のレイアウト

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
 import React from 'react'
-import './App.css';
+import './App.css'
+
+let tasks = [
+  {
+    "title": "新しいタスク",
+  }
+]
+
+let doneTasks = [
+  {
+    "title": "終了したタスク",
+  }
+]
 
 function App() {
+  var incompleteItems = []
+  for (var i in tasks) {
+    incompleteItems.push(
+      <div className="task-item">
+        <input type="checkbox"></input>
+        <span>{tasks[i].title}</span>
+      </div>)
+  }
+
+  var completeItems = []
+  for (var j in doneTasks) {
+    completeItems.push(
+      <div className="task-item">
+        <input type="checkbox" checked></input>
+        <span>{doneTasks[j].title}</span>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Header</p>
+        <p>Todo</p>
       </header>
+      <main>
+        <form>
+          <input placeholder="ここにタスク名を入力"></input>
+          <button>追加</button>
+        </form>
+        <div className="incomplete-items">
+          <h2>未完了</h2>
+          <ul>
+            {incompleteItems}
+          </ul>
+        </div>
+        <div className="done-items">
+          <h2>完了</h2>
+          <ul>
+            {completeItems}
+          </ul>
+        </div>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
