d6230e356b75d13bd6f0149d6d83dd322cf21164
datt16 <osatom.4639@gmail.com>
2021-04-16 11:54:14 +0000

    タスク追加できるようにした

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-let tasks = [
-  {
-    "title": "新しいタスク",
-  }
-]
+const Main = () => {
+  const [newTaskTitle, setTaskTitle] = useState("")
+  const [tasks, setTasks] = useState([])
 
-let doneTasks = [
-  {
-    "title": "終了したタスク",
+  const AddTask = (title) => {
+    setTasks([
+      ...tasks, {
+        "title": title,
+        "id": parseInt(Date.now() * Math.random()).toString()
+      }
+    ])
   }
-]
+
+  return (
+    <>
+      <h2>新規タスク</h2>
+      <form onSubmit={e => e.preventDefault()}>
+        <input placeholder="ここにタスク名を入力" name="newTaskTitle" value={newTaskTitle} onChange={(event) => setTaskTitle(event.target.value)}></input>
+        <button type="button" onClick={() => AddTask(newTaskTitle)}>追加</button>
+      </form>
+      <div className="incomplete-items">
+        <h2>未完了</h2>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <input type="checkbox"></input>
+              <span>{task.title}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="done-items">
+        <h2>完了</h2>
+        <ul>
+
+        </ul>
+      </div>
+    </>
+  )
+}
+
 
 function App() {
-  var incompleteItems = []
-  for (var i in tasks) {
-    incompleteItems.push(
-      <div className="task-item">
-        <input type="checkbox"></input>
-        <span>{tasks[i].title}</span>
-      </div>)
-  }
-
-  var completeItems = []
-  for (var j in doneTasks) {
-    completeItems.push(
-      <div className="task-item">
-        <input type="checkbox" checked></input>
-        <span>{doneTasks[j].title}</span>
-      </div>
-    )
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <p>Todo</p>
       </header>
-      <main>
-        <form>
-          <input placeholder="ここにタスク名を入力"></input>
-          <button>追加</button>
-        </form>
-        <div className="incomplete-items">
-          <h2>未完了</h2>
-          <ul>
-            {incompleteItems}
-          </ul>
-        </div>
-        <div className="done-items">
-          <h2>完了</h2>
-          <ul>
-            {completeItems}
-          </ul>
-        </div>
-      </main>
+      <Main />
     </div>
   )
 }
