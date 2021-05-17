1b45eec6a226bd79364b70ff9c52b47633650af7
datt16 <osatom.4639@gmail.com>
2021-05-17 23:30:22 +0900

    reducer結合してることを理解した

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import taskerReducer from "../features/task/taskSlice"
 import UserReducer from "../features/user/userSlice"
 
+const reducer = combineReducers({
+  tasker: taskerReducer,
+  user: UserReducer,
+})
+
 export default configureStore({
-  reducer: {
-    tasker: taskerReducer,
-    user: UserReducer,
-  },
+  reducer,
 })
