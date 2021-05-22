2d6967b53eb6a66ab3af76489e58102528deb3d2
datt16 <osatom.4639@gmail.com>
2021-05-22 10:47:03 +0900

    apiキーの書式変更

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,13 @@ import "firebase/firestore"
 import "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAtTZqK4gYPJlPnYg7AYncYVzIYCM67qr0",
+  authDomain: "datt-todo-0415.firebaseapp.com",
+  projectId: "datt-todo-0415",
+  storageBucket: "datt-todo-0415.appspot.com",
+  messagingSenderId: "800816684159",
+  appId: "1:800816684159:web:2b48a2209e969644c1734a",
+  measurementId: "G-EW3D7B6WXN",
 }
 
 firebase.initializeApp(firebaseConfig)
