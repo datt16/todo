f0288a829f679411026f143ff80d610900f0fc33
datt16 <osatom.4639@gmail.com>
2021-06-08 00:56:14 +0900

    ドロワーにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,88 @@
 import React from "react"
-import { Container } from "@material-ui/core"
+import {
+  Box,
+  Container,
+  createStyles,
+  Drawer,
+  Hidden,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core"
 import { Tasks } from "./features/task/Tasks"
-import { InputForm } from "./components/addTaskForm"
+import { InputForm } from "./components/AddTaskForm"
 import Auth from "./components/Auth"
 import { CustomAppBar } from "./components/Appbar"
 
-const App:React.FC = () => {
+import { useSelector } from "react-redux"
+
+import InboxIcon from "@material-ui/icons/Inbox"
+import { AppState } from "./app/store"
+import { UserAgentButton } from "./components/UserAgentButton"
+
+const drawerWidth = 260
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  })
+)
+
+const App: React.FC = () => {
+  const { uid } = useSelector((state: AppState) => state.user)
+  const classes = useStyles()
   return (
-    <div>
-      <CustomAppBar />
-      <Auth>
-        <div className={"App"}>
+    <div className={classes.root}>
+      {/* <CustomAppBar /> */}
+      <div className={classes.drawer}>
+        <Drawer
+          variant="permanent"
+          open
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <List>
+            <Box py="3" display="flex" justifyContent="center" alignContent="center">
+              <Typography variant="h5">タスク管理アプリ</Typography>
+            </Box>
+            <ListItem button key={"text"}>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={"text"} />
+            </ListItem>
+          </List>
+          <UserAgentButton uid={uid} />
+        </Drawer>
+      </div>
+      <div className={classes.content}>
+        <Auth>
           <Container maxWidth="sm">
             <InputForm />
             <Tasks />
           </Container>
-        </div>
-      </Auth>
+        </Auth>
+      </div>
     </div>
   )
 }
