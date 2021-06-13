948af06b73a4087878341f80eca95e39802cad10
datt16 <osatom.4639@gmail.com>
2021-06-13 20:47:58 +0900

    最初の画面に注意点追加

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -5,6 +5,8 @@ import firebase from "../plugins/Firebase"
 import { Login } from "../features/user/userSlice"
 import { AppState } from "../app/store"
 
+import { Box, Container, Link, Typography } from "@material-ui/core"
+
 type AuthPropsType = {
   children: ReactNode
 }
@@ -37,7 +39,41 @@ const Auth: React.FC<AuthPropsType> = (props: AuthPropsType) => {
 
   return (
     <div>
-      {signed ? <div>{props.children}</div> : <p>ログインしてください</p>}
+      {signed ? (
+        <div>{props.children}</div>
+      ) : (
+        <div>
+          <Container>
+            <Box alignContent="center">
+              <Box display="flex" justifyContent="center" my={2}>
+                <Typography variant="h1">Todo</Typography>
+              </Box>
+              <Box display="flex" justifyContent="center">
+                <Box pr={3}>
+                  <Typography variant="body1">作成者 : datt16</Typography>
+                </Box>
+                <Box>
+                  <Typography variant="body1">
+                    リポジトリ :
+                    <Link href="https://github.com/datt16/todo"> GitHub</Link>
+                  </Typography>
+                </Box>
+              </Box>
+              <Box display="flex" justifyContent="center" my={2}>
+                <Typography variant="body1">
+                  利用にはGoogleアカウントが必要になります
+                  <br />
+                  このアプリはまだ開発中のため、開発者によって入力情報の
+                  <b>読み取り・削除・変更</b>が行われる可能性があります
+                  <br />
+                  アカウント情報は、
+                  <u>名前・アイコン・識別 IDの取得以外の用途には使用しません</u>
+                </Typography>
+              </Box>
+            </Box>
+          </Container>
+        </div>
+      )}
     </div>
   )
 }
